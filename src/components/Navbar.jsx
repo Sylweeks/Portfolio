ed490c6c50d78300a4ff3599e468c08ded02d34e
Sylwek <sylwester.szwarczynski@gmail.com>
2025-09-25 20:57:50 +0200

    End of Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,49 @@
 import { Link } from 'react-router-dom'
 import NavbarCss from '../styles/Navbar.module.css'
+import { useState } from 'react'
 
 function Navbar() {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen)
+	}
 	return (
-		<nav>
+		<nav className={NavbarCss.navbar}>
 			<div className={NavbarCss.navbarContainer}>
 				<h1>Sylwester Szwarczyński</h1>
-				<ul>
+				<ul className={`${NavbarCss.navLinks} ${isOpen ? NavbarCss.active : ''}`}>
 					<li>
-						<Link to='/about'>O mnie</Link>
+						<Link to='/about' onClick={toggleMenu}>
+							About me
+						</Link>
 					</li>
 					<li>
-						<Link to='/skills'>Umiejętności</Link>
+						<Link to='/skills' onClick={toggleMenu}>
+							Skills
+						</Link>
 					</li>
 					<li>
-						<Link to='/projects'>Projekty</Link>
+						<Link to='/projects' onClick={toggleMenu}>
+							Projects
+						</Link>
 					</li>
 					<li>
-						<Link to='/contact'>Kontakt</Link>
+						<Link to='/contact' onClick={toggleMenu}>
+							Contact
+						</Link>
 					</li>
-					<li>
-						<a href='git-sheet.pdf' download>
-							CV
+					<li className={NavbarCss.CVButton}>
+						<a href='git-sheet.pdf' download onClick={toggleMenu}>
+							Download CV
 						</a>
 					</li>
 				</ul>
+				<div className={`${NavbarCss.burger} ${isOpen ? NavbarCss.toggle : ''}`} onClick={toggleMenu}>
+					<div className={NavbarCss.line1}></div>
+					<div className={NavbarCss.line2}></div>
+					<div className={NavbarCss.line3}></div>
+				</div>
 			</div>
 		</nav>
 	)
